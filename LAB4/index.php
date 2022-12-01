5b30aad30fc1aa2c08009577b156f91e045b2107
SUTHIDA-042 <640112418042@bru.ac.th>
2022-12-01 15:27:47 +0700

    LAB3

---
+++ LAB4/index.php
@@ -0,0 +1,28 @@
+<?php
+echo "HELLO PHP !!";
+
+phpinfo();
+
+$no = 10;
+$name = 'win';
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>dang</td>
+        </tr>
+        <?php
+        for ($i = 2; $i < 5; $i++){
+        ?>
+        <tr>
+            <td><?php echo $i ?></td>
+            <td><?= $name ?></td>
+        }
